c1cd148522af188233c05b850bdbf636e1bbc3c0
Johannes Rudolph <jrudolph@meshcloud.io>
2021-12-27 09:39:26 +0100

    Add requests as an explicit runtime dependency
    split dev and runtime requirements, keep them in sync by building setup.py
    install_requires from requirements.txt

--- setup.py
+++ setup.py
@@ -16,13 +16,19 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    install_requires = f.read().strip().split("\n")
+
+with open(path.join(here, 'dev-requirements.txt'), encoding='utf-8') as f:
+    dev_requires = f.read().strip().split("\n")
+
 setup(
     name='promalyze',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.2',
+    version='0.0.3',
 
     description='A a library for querying prometheus and working with the data',
     long_description=long_description,
@@ -73,13 +79,13 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pandas'],
+    install_requires=install_requires,
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'test': ['coverage', 'nose']
+        'dev': dev_requires
     }
 )
